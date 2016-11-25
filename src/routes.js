7cb5be77dc8b1a626a01a3dac066e4e6326b10b6
elk11 <elvar@europe.com>
2016-11-25 22:14:23 +0000

    Tonleikar!
    náum í tónleika í stað dagskráa

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const title = 'Dagskráin';
+  const title = 'Tónleikar';
 
   scheduleApi
-    .channels()
-    .then((channels) => {
-      res.render('index', { title, channels });
+    .concerts()
+    .then((concerts) => {
+      res.render('index', { title, concerts });
     })
     .catch((error) => {
       const message = 'Villa við að sækja stöðvar';
@@ -18,20 +18,5 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/tv/:name', (req, res) => {
-  const name = req.params.name || '';
-
-  scheduleApi
-    .channel(name)
-    .then((schedule) => {
-      const title = 'Dagksrá stöðvar';
-
-      res.render('channel', { title, schedule });
-    })
-    .catch((error) => {
-      const message = 'Villa við að sækja stöð';
-      res.render('error', { message, error });
-    });
-});
 
 module.exports = router;
